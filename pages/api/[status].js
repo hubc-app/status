0589bd122f698864e74759082c50869c95d54530
Dor Kleiman <configurator@gmail.com>
2021-01-28 15:55:16 +0200

    :wrench: Prettier

--- pages/api/[status].js
+++ pages/api/[status].js
@@ -1,5 +1,7 @@
-module.exports = ({query: { status }},res) => {
-    res.status(status)
-    res.setHeader('Content-Type', 'text/html')
-    res.send(`<body style="background: black url(https://http.cat/${status}) no-repeat; background-size: contain; background-position: center" />`)
-}
+module.exports = ({ query: { status } }, res) => {
+	res.status(status);
+	res.setHeader('Content-Type', 'text/html');
+	res.send(
+		`<body style="background: black url(https://http.cat/${status}) no-repeat; background-size: contain; background-position: center" />`,
+	);
+};
